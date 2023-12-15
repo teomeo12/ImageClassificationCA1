a9f78f94d9f471dcb5bf715670c22e5c20551bff
Vincent Arellano <67905295+VincentArellano@users.noreply.github.com>
2023-12-15 16:53:27 +0000

    :sparkles: Add dataset histogram
    Create a function to plot a histogram of the number of data for each class.

--- combined_trees.py
+++ combined_trees.py
@@ -3,7 +3,6 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-import cv2
 
 # Deep Learning Framework
 import tensorflow as tf
@@ -39,17 +38,24 @@ def load_cifar10_filtered(selected_classes, validation_split=0.1):
 def load_cifar100_filtered(selected_classes, validation_split=0.1, cifar10_classes=10):
     (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
 
-    y_train += cifar10_classes
-    y_test += cifar10_classes
+    # Define a mapping for all tree classes to a single label
+    tree_label = 101  # You can choose any label not used in CIFAR-100
 
-    train_mask = np.isin(y_train, selected_classes).reshape(-1)
-    test_mask = np.isin(y_test, selected_classes).reshape(-1)
+    # Set the tree_label for all tree classes
+    y_train_filtered = np.where(np.isin(y_train, [47,52,56,59,96]), tree_label, y_train)
+    y_test_filtered = np.where(np.isin(y_test, [47,52,56,59,96]), tree_label, y_test)
+
+    y_train_filtered += cifar10_classes
+    y_test_filtered += cifar10_classes
+
+    train_mask = np.isin(y_train_filtered, [tree_label] + selected_classes).reshape(-1)
+    test_mask = np.isin(y_test_filtered, [tree_label] + selected_classes).reshape(-1)
 
     x_train_filtered = x_train[train_mask]
-    y_train_filtered = y_train[train_mask]
+    y_train_filtered = y_train_filtered[train_mask]
 
     x_test_filtered = x_test[test_mask]
-    y_test_filtered = y_test[test_mask]
+    y_test_filtered = y_test_filtered[test_mask]
 
     x_train_filtered, x_valid_filtered, y_train_filtered, y_valid_filtered = train_test_split(
         x_train_filtered, y_train_filtered, test_size=validation_split, random_state=42
@@ -73,11 +79,13 @@ def combine_cifar_datasets(x_train_cifar10, y_train_cifar10, x_valid_cifar10, y_
     return (x_train_combined, y_train_combined), (x_valid_combined, y_valid_combined), (x_test_combined, y_test_combined)
 
 def visualize_images(X, y, class_labels, class_names):
+    num_of_samples = []
     cols = 5
     num_classes = len(class_labels)
 
     fig, axs = plt.subplots(nrows=num_classes, ncols=cols, figsize=(5, 50))
     fig.tight_layout()
+
     for i in range(cols):
         for j, class_label in enumerate(class_labels):
             x_selected = X[np.where(y == class_label)[0]]
@@ -87,28 +95,35 @@ def visualize_images(X, y, class_labels, class_names):
                 axs[j][i].axis("off")
 
                 if i == 2 and j < len(class_names):
+                    num_of_samples.append(len(x_selected))
                     axs[j][i].set_title(f"{j} - {class_names[j]}")
     plt.show()
 
-def plot_distribution(y, class_labels):
-    num_of_samples = []
+def grayscale(img):
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  return img
 
-    for class_label in class_labels:
-        x_selected = y[y == class_label]
-        num_of_samples.append(len(x_selected))
+def equalize(img):
+  img = cv2.equalizeHist(img)
+  return img
 
-    plt.figure(figsize=(12, 4))
-    plt.bar(range(0, len(class_labels)), num_of_samples)
-    plt.title("Distribution of the training set")
-    plt.xlabel("Class number")
-    plt.ylabel("Number of images")
-    plt.show()
+def scale_down_variance(X_train, X_test):
+    # Scale down the variance
+    X_train = X_train/255
+    X_test = X_test/255
+    return X_train, X_test
+
+def preprocessing(img):
+  img = grayscale(img)
+  img = equalize(img)
+  img = scale_down_variance(img)
+  return img
 
 def main():
     classes_cifar10 = [1, 2, 3, 4, 5, 7, 9]
     class_names_cifar10 = ['automobile', 'bird', 'cat', 'deer', 'dog', 'horse', 'truck']
-    classes_cifar100 = [12, 18, 21, 23, 29, 44, 45, 51, 56, 57, 58, 62, 66, 68, 69, 75, 90, 99, 100, 106, 108]
-    class_names_cifar100 = ['baby', 'bicycle', 'boy', 'bus', 'cattle', 'fox', 'girl', 'lawn-mower', 'man', 'maple tree', 'motorcycle', 'oak tree', 'palm tree', 'pickup truck', 'pine tree', 'rabbit', 'squirrel', 'tractor', 'train', 'willow tree', 'woman']
+    classes_cifar100 = [12, 18, 21, 23, 29, 44, 45, 51, 56, 58, 68, 75, 90, 99, 100, 108, 111]
+    class_names_cifar100 = ['baby', 'bicycle', 'boy', 'bus', 'cattle', 'fox', 'girl', 'lawn mower', 'man', 'motorcycle', 'pickup truck', 'rabbit', 'squirrel', 'tractor', 'train', 'woman', 'trees']
     combined_classes = classes_cifar10 + classes_cifar100
     combined_classes_name = class_names_cifar10 + class_names_cifar100
 
@@ -118,7 +133,7 @@ def main():
     (x_train_cifar100, y_train_cifar100), (x_valid_cifar100, y_valid_cifar100), (x_test_cifar100, y_test_cifar100) = \
         load_cifar100_filtered(classes_cifar100)
 
-    (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = \
+    (x_train_combined, y_train_combined), (x_valid_combined, y_valid_combined), (x_test_combined, y_test_combined) = \
         combine_cifar_datasets(x_train_cifar10, y_train_cifar10, x_valid_cifar10, y_valid_cifar10,
                                x_test_cifar10, y_test_cifar10, x_train_cifar100, y_train_cifar100,
                                x_valid_cifar100, y_valid_cifar100, x_test_cifar100, y_test_cifar100)
@@ -140,12 +155,12 @@ def main():
     print("Test labels:", y_test_cifar100.shape)
 
     print("Combined CIFAR datasets shapes:")
-    print("Combined Train data:", x_train.shape)
-    print("Combined Train labels:", y_train.shape)
-    print("Combined Validation data:", x_valid.shape)
-    print("Combined Validation labels:", y_valid.shape)
-    print("Combined Test data:", x_test.shape)
-    print("Combined Test labels:", y_test.shape)
+    print("Combined Train data:", x_train_combined.shape)
+    print("Combined Train labels:", y_train_combined.shape)
+    print("Combined Validation data:", x_valid_combined.shape)
+    print("Combined Validation labels:", y_valid_combined.shape)
+    print("Combined Test data:", x_test_combined.shape)
+    print("Combined Test labels:", y_test_combined.shape)
 
     assert x_train_cifar10.shape[0] == y_train_cifar10.shape[0], "The number of training images in CIFAR-10 is different from the number of labels"
     assert x_valid_cifar10.shape[0] == y_valid_cifar10.shape[0], "The number of validation images in CIFAR-10 is different from the number of labels"
@@ -161,15 +176,14 @@ def main():
     assert x_valid_cifar100.shape[1:] == (32, 32, 3), "The validation images in CIFAR-100 are not 32x32x3"
     assert x_test_cifar100.shape[1:] == (32, 32, 3), "The test images in CIFAR-100 are not 32x32x3"
 
-    assert x_train.shape[0] == y_train.shape[0], "The number of combined training images is different from the number of labels"
-    assert x_valid.shape[0] == y_valid.shape[0], "The number of combined validation images is different from the number of labels"
-    assert x_test.shape[0] == y_test.shape[0], "The number of combined test images is different from the number of labels"
-    assert x_train.shape[1:] == (32, 32, 3), "The combined training images are not 32x32x3"
-    assert x_valid.shape[1:] == (32, 32, 3), "The combined validation images are not 32x32x3"
-    assert x_test.shape[1:] == (32, 32, 3), "The combined test images are not 32x32x3"
+    assert x_train_combined.shape[0] == y_train_combined.shape[0], "The number of combined training images is different from the number of labels"
+    assert x_valid_combined.shape[0] == y_valid_combined.shape[0], "The number of combined validation images is different from the number of labels"
+    assert x_test_combined.shape[0] == y_test_combined.shape[0], "The number of combined test images is different from the number of labels"
+    assert x_train_combined.shape[1:] == (32, 32, 3), "The combined training images are not 32x32x3"
+    assert x_valid_combined.shape[1:] == (32, 32, 3), "The combined validation images are not 32x32x3"
+    assert x_test_combined.shape[1:] == (32, 32, 3), "The combined test images are not 32x32x3"
 
-    visualize_images(x_train, y_train, combined_classes, combined_classes_name)
-    plot_distribution(y_train, combined_classes)
+    visualize_images(x_train_combined, y_train_combined, combined_classes, combined_classes_name)
 
 if __name__ == "__main__":
     main()
